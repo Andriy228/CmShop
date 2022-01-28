b5a5e3cc51ab0b16969e4bd2d48f3a681850deae
Yatsevyuk Andriy <andriy2004fz@gmail.com>
2022-01-28 13:26:42 +0300

    FirstCommit!

---
+++ BLL/Repositories/Classes/AuthorRepository.cs
@@ -0,0 +1,44 @@
+﻿using BLL.Repositories.Interfaces;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Repositories.Classes
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        public void Add(Author entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Author entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Author> FindMany(Expression<Func<Author, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author FindOne(Expression<Func<Author, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Author> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BLL/Repositories/Classes/ComicsRepository.cs
@@ -0,0 +1,44 @@
+﻿using BLL.Repositories.Interfaces;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Repositories.Classes
+{
+    public class ComicsRepository : IComicsRepository
+    {
+        public void Add(Comics entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Comics entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comics FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comics> FindMany(Expression<Func<Comics, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comics FindOne(Expression<Func<Comics, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comics> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
